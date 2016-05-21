d42ccd8c69ce6422c8786d2f62716927a54878c6
fabienduhamel <fabienduhamel@users.noreply.github.com>
2016-05-21 11:23:22 +0200

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Starting Symfony App
+
+- Symfony 3.0.x
+- Bootstrap
+- Gulp concat
+- Gulp minify
+- Gulp browserSync
---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var cssmin = require('gulp-cssmin');
+var browserSync = require('browser-sync').create();
+
+var config = {
+    localServerUrl: "127.0.0.1:8000/app_dev.php",
+};
+
+gulp.task('css', function () {
+    return gulp.src([
+            'bower_components/bootstrap/dist/css/bootstrap.min.css',
+            'app/Resources/css/**/*.css'
+        ])
+        .pipe(concat('all.css'))
+        .pipe(gulp.dest('web/dist/css'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('minCss', function () {
+    return gulp.src([
+            'web/dist/css/all.css'
+        ])
+        .pipe(cssmin())
+        .pipe(gulp.dest('web/dist/css'));
+});
+
+gulp.task('watch', function () {
+    browserSync.init({
+        proxy: config.localServerUrl
+    });
+    gulp.watch('app/Resources/css/*.css', ['css']);
+    gulp.watch("app/Resources/views/**/*.html.twig").on('change', browserSync.reload);
+});
+
+gulp.task('default', ['css', 'watch']);
+gulp.task('min', ['css', 'minCss']);
